cbbe0607fa3f5f7a885f7d034a9297e23d623f06
codesbeforebros <norib22@hotmail.com>
2023-03-01 12:35:42 -0700

    Next.js Env don't work in context provider :(

--- pages/_openElections.js
+++ pages/_openElections.js
@@ -1,16 +1,18 @@
 import { useState, useEffect, useContext } from 'react';
 import { contractAddress, contractABI} from '../api/_connectionConst'
 import Election from './_election';
-import { InfuraContext } from '@/api/_infuraProvider';
+import Web3 from "web3";
+//import { InfuraContext } from '@/api/_infuraProvider';
 
 function OpenElections(props) {
   const [events, setEvents] = useState(null);
-  const infura = useContext(InfuraContext);
+  //const contract = useContext(InfuraContext);
 
   useEffect(() => {
     async function getOpenElections() { 
       try {
-        const contract = new infura.eth.Contract(contractABI, contractAddress); //new web3.eth.Contract(contractABI, contractAddress); 
+        const web3 = new Web3("https://sepolia.infura.io/v3/" + process.env.NEXT_PUBLIC_INFURA_KEY); //goerli, mainnet
+        const contract = new web3.eth.Contract(contractABI, contractAddress); 
         const options = { fromBlock: '2990000', toBlock: 'latest' };
         const events = await contract.getPastEvents("ElectionCreated", options);
         setEvents(events);
