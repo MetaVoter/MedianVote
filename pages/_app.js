1f8aee903c4beee5aa0382723b1a6b055001dfa0
codesbeforebros <norib22@hotmail.com>
2023-03-03 11:34:24 -0700

    Split into electionResults and voteOnElection

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
