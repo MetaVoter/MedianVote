8c18cd5146246bb7185b78b6f5e1554337209509
codesbeforebros <norib22@hotmail.com>
2023-03-04 11:11:12 -0700

    Removing WalletInfo componet, replaced by Wallet Connect

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import WalletInfo from './_walletInfo'
 import OpenElections from './_openElections'
 import { Web3Provider } from '../api/_web3Provider'
 
@@ -18,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>        
+      <main className={styles.main}>  
         <div>
           <Web3Button icon="show" label="Connect Wallet" balance="show" />
           <br />
@@ -31,8 +29,5 @@ export default function Home() {
 }
 
 /*<Web3Provider>
-            <WalletInfo/>
-            <InfuraProvider>
               <OpenElections/>
-            </InfuraProvider>
         </Web3Provider>*/
