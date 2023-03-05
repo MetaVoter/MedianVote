51c4c48b0b5b697341a9b6e6e08ed21f3fdfbc5d
codesbeforebros <norib22@hotmail.com>
2023-03-05 05:54:40 -0700

    Styling for SBF

--- pages/SBF.js
+++ pages/SBF.js
@@ -1,4 +1,5 @@
-import PageTemplate from "./_pageTemplate"
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
 import GetElectionResults from "./_getElectionResults";
 
 const supportedElections = [
@@ -7,10 +8,17 @@ const supportedElections = [
 
 function SBF() {
     return (
-      <div>
-        <img src="/sbf_hair.gif" />
-        <GetElectionResults supportedElections={supportedElections}/>
-      </div>
+      <>
+        <Head>
+          <title>Median Vote</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>
+          <img src="/sbf_hair.gif" />
+          <GetElectionResults supportedElections={supportedElections}/>
+        </main>
+      </>
     );
 }
 
