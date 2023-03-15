67d086c8d4c51934dfaf65dd74182a6668b97d2a
codesbeforebros <norib22@hotmail.com>
2023-03-14 20:22:14 -0700

    Front page with links to polls and results

--- pages/SBF.js
+++ pages/SBF.js
@@ -1,28 +1,14 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-import GetElectionResults from "./_getElectionResults";
-import { useRouter } from 'next/router';
+import PageTemplate from "./_pageTemplate"
+import ElectionsToVoteOn from "./_electionsToVoteOn";
 
 const supportedElections = [
-  "0x171aa729ae009b3d66ca96fbe7f3deb88437470180cc2df2bad1671ed8420e2c"
-];
+    "0x171aa729ae009b3d66ca96fbe7f3deb88437470180cc2df2bad1671ed8420e2c"
+  ];
 
 function SBF() {
-  const router = useRouter();
-  const interval = router.query["interval"];
-  return (
-    <>
-      <Head>
-        <title>Median Vote</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <img src="/sbf_hair.gif" />
-        <GetElectionResults supportedElections={supportedElections} interval={interval}/>
-      </main>
-    </>
-  );
+    return (
+      <PageTemplate ComponentToRender={ElectionsToVoteOn} supportedElections={supportedElections} />
+    );
 }
 
 export default SBF;
